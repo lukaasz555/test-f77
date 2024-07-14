8dd9bde40fbaf17d0badb5a2974ffb7817c29950
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-07-14 10:57:46 +0200

    chore: update tailwind config, add clsx, twmerge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+		borderRadius: {
+			sm: '0.125rem',
+			md: '0.375rem',
+			lg: '0.5rem',
+			xl: '0.75rem',
+			'2xl': '1rem',
+			none: '0',
+		},
+		screens: {
+			xs: '0px',
+			sm: '600px',
+			md: '900px',
+			lg: '1200px',
+			xl: '1536px',
+		},
+	},
+	plugins: [],
 };
 export default config;
